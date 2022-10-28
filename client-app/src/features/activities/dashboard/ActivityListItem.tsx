37eb97fd7a0daade797b38fc2b99ab456002cb0b
Duncan Crawford <Duncan@marginfuel.com>
2022-10-29 06:08:43 +1300

    End of Section 9

---
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -0,0 +1,56 @@
+import { SyntheticEvent, useState } from 'react'
+import { Link } from 'react-router-dom'
+import { Button, Icon, Item, Segment } from 'semantic-ui-react'
+import { Activity } from '../../../app/models/activity'
+
+interface Props {activity: Activity}
+
+export default function ActivityListItem({activity}: Props) {
+   
+   
+    
+
+    return(
+     <Segment.Group>
+<Segment>
+    <Item.Group>
+        <Item>
+          <Item.Image size='tiny' circular src='/assets/user.png'/>
+          <Item.Header as={Link} to={`/activities/${activity.id}`}>
+        {activity.title}</Item.Header>
+        <Item.Description>
+            Hosted By Bob
+        </Item.Description>
+
+        </Item>
+    </Item.Group>
+</Segment>
+<Segment>
+<span>
+    <Icon name='clock'/>{activity.date}
+    <Icon name='marker'/>{activity.venue}
+</span>
+
+<Segment secondary>
+Attendies Go Here
+</Segment>
+<Segment clearing>
+    <span>{activity.description}</span>
+    <Button
+    as={Link}
+    to={`/activities/${activity.id}`}
+    color='teal'
+    floated='right'
+    content='View'
+    />
+</Segment>
+
+
+
+
+
+
+</Segment>
+     </Segment.Group>
+    )
+}
